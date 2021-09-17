b8f479e87c9500af27e60d8121a2d1f358d5e161
iwswordpress <iwswordpress@gmail.com>
2021-09-17 13:58:02 +0100

    prod build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: ["./src/**/*.jsx", "./index.html"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: "#3fbaeb",
+          DEFAULT: "#0fa9e6",
+          dark: "#0c87b8",
+        },
+      },
+      fontFamily: {
+        headline: "Poppins, sans-serif",
+      },
+    },
   },
   variants: {
     extend: {
